0711bf6c5934fba3a09b4b6c3e9fc461d83a5ef0
Guilherme Afonso <62111048+guilhermeafonsoch@users.noreply.github.com>
2022-09-14 22:19:30 -0300

    Delete secret.php

--- secret.php
+++
@@ -1,8 +0,0 @@
-<?php 
-const HOSTNAME = "petwash.cjpk81kuqb7j.us-east-1.rds.amazonaws.com";
-const USERNAME = "root";
-const PORT = "3306";
-const PASSWORD = "^aNaHgGzc*M4";
-$dbName = "petwash";
-$table = "petwash.customer";
-?>
