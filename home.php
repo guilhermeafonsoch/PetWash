1d0555ad95d36d6c36acd688471a0b8a7f66b0e3
EduardoGoncalvesProg <eduardo.goncalves.2002@gmail.com>
2022-09-30 19:10:57 -0300

    mostra dados do usuario do banco

--- home.php
+++ home.php
@@ -1,10 +1,23 @@
 <?php
-// session_destroy();
+
 session_start();
+
+if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == true))
+{
+  header('location:index.html');
+}
+
+$logado = $_SESSION['login'];
+
 include("functions.php");
 
-$username = select_username($_SESSION['login']);
-// valida_sessao();
+$where= "email = " . "'" . $_SESSION['login'] . "';";
+
+$dados_usuario = selectDB($dbName, $table, $campos = '*', $where);
+
+$username = $dados_usuario[0]["username"];
+$email = $dados_usuario[0]["email"];
+
 ?>
 
 <!DOCTYPE html>
@@ -13,41 +26,37 @@ $username = select_username($_SESSION['login']);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/styles/home.css">
     <title>HOME</title>
 </head>
 <body>
-    <h1>Bem vindo, <?php echo $username;?>!</h1>
     <div class="row">
-        <div class="col-4">
+        <div class="col-12">
+            <h1>Bem vindo!</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
             <div class="card" style="width: 18rem;">  
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Vestibulum at eros</li>
+                <li class="list-group-item">Usuário</li>
+                <li class="list-group-item"><?php echo $username; ?></li>
             </ul>
+            </div>
         </div>
-</div>
-        <div class="col-4">
+    </div>
+    <div class="row">
+        <div class="col-12">
             <div class="card" style="width: 18rem;">  
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Vestibulum at eros</li>
+                <li class="list-group-item">Email</li>
+                <li class="list-group-item"><?php echo $email; ?></li>
             </ul>
-        </div>
-        </div>
-        <div class="col-4">
-            <div class="card" style="width: 18rem;">  
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Vestibulum at eros</li>
-            </ul>
-        </div>
+            </div>
         </div>
     </div>
 </body>
--- valida_login.php
+++ valida_login.php
@@ -1,5 +1,5 @@
 <?php
-// session_destroy();
+
 session_start();
 
 include("functions.php");
